b573fcf2a51baa54ab35c1d8ff8b5198a81a3e98
carlopizzuto <carlo.pizzutoa@gmail.com>
2024-02-15 02:42:46 -0600

    started 2fa

--- data/user.ts
+++ data/user.ts
@@ -2,25 +2,21 @@ import { db } from "@/lib/db";
 
 export const getUserByEmail = async (email: string) => {
   try {
-    const user = await db.user.findUnique({ where: { email }});
+    const user = await db.user.findUnique({ where: { email } });
 
     return user;
   } catch {
-    null;
-  }
-};
-
-export const getUserById = async (id: string | undefined) => {
-  try {
-    if (id === undefined) {
-      // Handle the case where id is undefined, or throw an error
-      throw new Error("id is undefined");
-    }
-    const user = await db.user.findUnique({ where: { id } });
-    return user;
-  } catch (error) {
-    // Handle or log the error appropriately
-    console.error(error);
     return null;
   }
 };
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { id } });
+
+    return user;
+  } catch {
+    return null;
+  }
+};
+
