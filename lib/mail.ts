033b38e5f7b859358ad474b579ac3924b67be7e3
carlopizzuto <carlo.pizzutoa@gmail.com>
2024-02-15 01:09:02 -0600

    added email verification

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ export const sendVerificationEmail = async (
     email: string, 
     token: string
     ) => {
-        const confirmationLink = `http://localhost:300/auth/new-verification?token=${token}`;
+        const confirmationLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
         await resend.emails.send({
             from: "onboarding@resend.dev",
